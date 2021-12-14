e2d1baf65743bc675a61d1009cb55aecfd460f4f
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-14 15:46:57 -0700

    commit tree 67ccdfcf44b1b43bdd94e08c9131388426b6d50c

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+    # this is an instance method
+    def bark 
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit
+
+class Person 
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk 
+        puts "The Person is walking"
+    end
+end
